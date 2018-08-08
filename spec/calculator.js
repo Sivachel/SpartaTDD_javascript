01f52bd6ec1995b5bdb6d983fcd948fa3d070264
Sivachel <lsivachelvan@spartaglobal.com>
2018-08-08 16:18:05 +0100

    init

--- spec/calculator.js
+++ spec/calculator.js
@@ -1,6 +1,6 @@
-escribe("Intro to doing TDD in javascript", function () {
+describe("Intro to doing TDD in javascript", function () {
 
-  var calculator = require("../calculator/function")
+  var calculator = require("../calculator/function");
 
 it("should add numbers together", function (){
   expect(calculator.addTwoNumbers(3,2)).toEqual(5);
